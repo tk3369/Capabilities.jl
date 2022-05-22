487a7a73439ac7e2063cf6b6f9c5f56939d8f9c9
Tom Kwong <tk3369@gmail.com>
2022-05-22 09:31:42 -0700

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,29 @@ using Test
 
 @importcap Capabilities [rand, io]
 @defcap secret
+@defcap super_secret secret
 
 @testset "Capabilities.jl" begin
 
     @testset "Positive tests" begin
+        # Call a function with a subset of capabilities
         @cap [rand, io] foo() = bar()
         @cap [rand] bar() = rand()
-        @test_nowarn foo()  # foo has more capabilities
+        @test_nowarn foo()
 
-        # anon function
-        lambda = @cap([rand, io], () -> rand())
-        @test_nowarn lambda()
+        # The same thing works for anonymous functions.
+        f = @cap([rand, io], () -> bar())
+        @test_nowarn f()
     end
 
     @testset "Negative tests" begin
+        # Not allowed to call a function with a different set of capabilities.
         @cap [rand] baz() = wat()
         @cap [secret] wat() = 2
         @test_throws IncapableError baz()
+
+        # Not allowed to call a funciton with more capabilities
+        @cap [super_secret] ss() = wat()
+        @test_throws IncapableError ss()
     end
 end
